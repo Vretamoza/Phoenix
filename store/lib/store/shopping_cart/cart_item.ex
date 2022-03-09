aeef387e7d8110baacdae55b69f6f39e6efce81b
Valery Retamoza <vretamoza@uninorte.edu.co>
2022-03-09 01:17:10 -0500

    Phoenix: from chapter 13 to 15

---
+++ store/lib/store/shopping_cart/cart_item.ex
@@ -0,0 +1,22 @@
+defmodule Store.ShoppingCart.CartItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "cart_items" do
+    field :price_when_carted, :decimal
+    field :quantity, :integer
+
+    belongs_to :cart, Store.ShoppingCart.Cart
+    belongs_to :product, Store.Catalog.Product
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(cart_item, attrs) do
+    cart_item
+    |> cast(attrs, [:price_when_carted, :quantity])
+    |> validate_required([:price_when_carted, :quantity])
+    |> validate_number(:quantity, greater_than_or_equal_to: 0, less_than: 100)
+  end
+end
